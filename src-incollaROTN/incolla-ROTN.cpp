1c0fde29e8049a2f80976105ba479fd24a69d67a
RedYouMan <rosturco.na@gmail.com>
2026-09-09 12:15:07 +0200

    agg usage

--- src-incollaROTN/incolla-ROTN.cpp
+++ src-incollaROTN/incolla-ROTN.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
   // 1. Controlla se l'utente ha passato il nome del file
   if (argc < 2)
   {
-    std::cout << "Usage: nomefile senza estensione appunti-da-incollare seguito da ctrl-Z + RETURN per terminare l'input" << std::endl;
+    std::cout << "Usage: nomefile senza estensione+RETURN Poi appunti-da-incollare + ctrl-Z + RETURN" << std::endl;
     return 1;
   }
 
