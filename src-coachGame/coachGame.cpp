9b3ff6d6d1e2ef206d6d6fab6027343cf0a0fd3c
RedYouMan <rosturco.na@gmail.com>
2026-09-11 19:22:04 +0200

    scacchiera di Scacchi-it in SFML

--- src-coachGame/coachGame.cpp
+++ src-coachGame/coachGame.cpp
@@ -447,6 +447,7 @@ int main(int argc, char *argv[])
 {
     if (argc < 3)
     {
+
         std::cout << "Uso: coachGame\"<nome file partita con estensione> <colore (es. b o n)>" << std::endl;
         return 1;
     }
